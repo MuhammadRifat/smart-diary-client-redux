c7e14855fdeb3842315a32aba32024cf7644fbc0
Abid Hossain <hosainabid@gmail.com>
2021-05-28 11:23:09 +0600

    Front end Codes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import smartDiary from './images/diary_logo.png';
+import { useDispatch } from 'react-redux';
+
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-
+import { getPosts } from './actions/posts';
 import useStyles from './styles';
+import memories from './images/memories.png';
 
 const App = () => {
-    const classes = useStyles();
-    return (
-        <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Smart Diary</Typography>
-                <img className={classes.image} src={smartDiary} alt="Diary" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch">
-                        <Grid item xs={12} sm={7}>
-                            <Posts></Posts> 
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                        <Form></Form>
-                        </Grid>
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-                    </Grid>
-                </Container>
-            </Grow>
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Smart Diary</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
         </Container>
-    );
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
